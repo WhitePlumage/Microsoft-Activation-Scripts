4a914a078030ed583a1ac3aec6230b36e4fefad9
whiteplumage <wmnm2014138@gmail.com>
2025-05-19 17:11:50 +0800

    Update from massgrave.dev/get.ps1

--- (FROM_WEB) get.ps1
+++ (FROM_WEB) get.ps1
@@ -1,36 +1,56 @@
 # Check massgrave.dev for more details
 
-$ErrorActionPreference = "Stop"
-
 write-host
-Write-Host "The current command (irm https://massgrave.dev/get | iex) will be retired on Dec 31, 2024."
+Write-Host "The current command (irm https://massgrave.dev/get | iex) will be retired in the future."
 Write-Host -ForegroundColor Green "Use the new command (irm https://get.activated.win | iex) moving forward."
 write-host
 
-# Enable TLSv1.2 for compatibility with older clients for current session
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-$DownloadURL1 = 'https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/52d4c52dba8e29a3c1fb295c8946dbe6cf2f0239/MAS/All-In-One-Version-KL/MAS_AIO.cmd'
-$DownloadURL2 = 'https://bitbucket.org/WindowsAddict/microsoft-activation-scripts/raw/52d4c52dba8e29a3c1fb295c8946dbe6cf2f0239/MAS/All-In-One-Version-KL/MAS_AIO.cmd'
-$DownloadURL3 = 'https://codeberg.org/massgravel/Microsoft-Activation-Scripts/raw/commit/52d4c52dba8e29a3c1fb295c8946dbe6cf2f0239/MAS/All-In-One-Version-KL/MAS_AIO.cmd'
-
-$URLs = @($DownloadURL1, $DownloadURL2, $DownloadURL3)
-$ShuffledURLs = $URLs | Sort-Object { Get-Random }
-
-try {
-    $response = Invoke-WebRequest -Uri $ShuffledURLs[0] -UseBasicParsing
+$troubleshoot = 'https://massgrave.dev/troubleshoot'
+if ($ExecutionContext.SessionState.LanguageMode.value__ -ne 0) {
+    $ExecutionContext.SessionState.LanguageMode
+    Write-Host "Windows PowerShell is not running in Full Language Mode."
+    Write-Host "Help - https://gravesoft.dev/fix_powershell" -ForegroundColor White -BackgroundColor Blue
+    return
 }
-catch {
-    try {
-        $response = Invoke-WebRequest -Uri $ShuffledURLs[1] -UseBasicParsing
+
+function Check3rdAV {
+    $avList = Get-CimInstance -Namespace root\SecurityCenter2 -Class AntiVirusProduct | Where-Object { $_.displayName -notlike '*windows*' } | Select-Object -ExpandProperty displayName
+    if ($avList) {
+        Write-Host '3rd party Antivirus might be blocking the script - ' -ForegroundColor White -BackgroundColor Blue -NoNewline
+        Write-Host " $($avList -join ', ')" -ForegroundColor DarkRed -BackgroundColor White
     }
-    catch {
-        $response = Invoke-WebRequest -Uri $ShuffledURLs[2] -UseBasicParsing
-    }
+}
+
+function CheckFile { 
+    param ([string]$FilePath) 
+    if (-not (Test-Path $FilePath)) { 
+        Check3rdAV
+        Write-Host "Failed to create MAS file in temp folder, aborting!"
+        Write-Host "Help - $troubleshoot" -ForegroundColor White -BackgroundColor Blue
+        throw 
+    } 
+}
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$URLs = @(
+    'https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/a149fc5f2048b135c649a04da89e1b2f3178b449/MAS/All-In-One-Version-KL/MAS_AIO.cmd',
+    'https://dev.azure.com/massgrave/Microsoft-Activation-Scripts/_apis/git/repositories/Microsoft-Activation-Scripts/items?path=/MAS/All-In-One-Version-KL/MAS_AIO.cmd&versionType=Commit&version=a149fc5f2048b135c649a04da89e1b2f3178b449',
+    'https://git.activated.win/massgrave/Microsoft-Activation-Scripts/raw/commit/a149fc5f2048b135c649a04da89e1b2f3178b449/MAS/All-In-One-Version-KL/MAS_AIO.cmd'
+)
+
+foreach ($URL in $URLs | Sort-Object { Get-Random }) {
+    try { $response = Invoke-WebRequest -Uri $URL -UseBasicParsing; break } catch {}
+}
+
+if (-not $response) {
+    Check3rdAV
+    Write-Host "Failed to retrieve MAS from any of the available repositories, aborting!"
+    Write-Host "Help - $troubleshoot" -ForegroundColor White -BackgroundColor Blue
+    return
 }
 
 # Verify script integrity
-$releaseHash = '39961D29B07D59D54A709AA7B152269ADC6349752660A0C66D627B09DF18B9B6'
+$releaseHash = '99B9D8E20701DDCA02676146F7878ECC79BC403CB7B51FBB1B15B2D8D8BC64C4'
 $stream = New-Object IO.MemoryStream
 $writer = New-Object IO.StreamWriter $stream
 $writer.Write($response)
@@ -38,7 +58,7 @@ $writer.Flush()
 $stream.Position = 0
 $hash = [BitConverter]::ToString([Security.Cryptography.SHA256]::Create().ComputeHash($stream)) -replace '-'
 if ($hash -ne $releaseHash) {
-    Write-Warning "Hash ($hash) mismatch, aborting!`nReport this issue at https://massgrave.dev/troubleshoot"
+    Write-Warning "Hash ($hash) mismatch, aborting!`nReport this issue at $troubleshoot"
     $response = $null
     return
 }
@@ -53,16 +73,17 @@ foreach ($path in $paths) {
 
 $rand = [Guid]::NewGuid().Guid
 $isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
-$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\MAS_$rand.cmd" } else { "$env:TEMP\MAS_$rand.cmd" }
+$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\MAS_$rand.cmd" } else { "$env:USERPROFILE\AppData\Local\Temp\MAS_$rand.cmd" }
+Set-Content -Path $FilePath -Value "@::: $rand `r`n$response"
+CheckFile $FilePath
 
-$ScriptArgs = "$args "
-$prefix = "@::: $rand `r`n"
-$content = $prefix + $response
-Set-Content -Path $FilePath -Value $content
-
-# Set ComSpec variable for current session in case its corrupt in the system
 $env:ComSpec = "$env:SystemRoot\system32\cmd.exe"
-Start-Process cmd.exe "/c """"$FilePath"" $ScriptArgs""" -Wait
+$chkcmd = & $env:ComSpec /c "echo CMD is working"
+if ($chkcmd -notcontains "CMD is working") {
+    Write-Warning "cmd.exe is not working.`nReport this issue at $troubleshoot"
+}
+saps -FilePath $env:ComSpec -ArgumentList "/c """"$FilePath"" $args""" -Wait
+CheckFile $FilePath
 
-$FilePaths = @("$env:TEMP\MAS*.cmd", "$env:SystemRoot\Temp\MAS*.cmd")
+$FilePaths = @("$env:SystemRoot\Temp\MAS*.cmd", "$env:USERPROFILE\AppData\Local\Temp\MAS*.cmd")
 foreach ($FilePath in $FilePaths) { Get-Item $FilePath | Remove-Item }
